6dd9ab8a4d70b765b88b398382f1674fdb813ea9
hbuie112358 <hbuie112358@gmail.com>
2015-01-15 10:55:17 -0500

    adding test files

---
+++ testFiles/toggle1Sec/toggle1sec.asm
@@ -0,0 +1,69 @@
+;;;;;;; toggle1sec.asm ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;;;;;;; Program hierarchy ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; Mainline
+;  
+;
+;;;;;;; Assembler directives ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+        list  P=PIC18F452, F=INHX32, C=160, N=0, ST=OFF, MM=OFF, R=DEC, X=ON
+        #include p18f452.inc
+        __CONFIG  _CONFIG1H, _HS_OSC_1H  ;HS oscillator
+        __CONFIG  _CONFIG2L, _PWRT_ON_2L & _BOR_ON_2L & _BORV_42_2L  ;Reset
+        __CONFIG  _CONFIG2H, _WDT_OFF_2H  ;Watchdog timer disabled
+        __CONFIG  _CONFIG3H, _CCP2MX_ON_3H  ;CCP2 to RC1 (rather than to RB3)
+        __CONFIG  _CONFIG4L, _LVP_OFF_4L  ;RB5 enabled for I/O
+
+;;;;;;; Variables ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+			cblock  0x000           ;Beginning of Access RAM
+			temp
+			temp1
+			temp2
+			endc
+
+;;;;;;; Macro definitions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;; Vectors ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+        org  0x0000             ;Reset vector
+        nop
+        goto  Mainline
+
+        org  0x0008             ;High priority interrupt vector
+        goto  $                 ;Trap
+
+        org  0x0018             ;Low priority interrupt vector
+        goto  $                 ;Trap
+
+;;;;;;; Mainline program ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+Mainline
+		movlw		0
+		movwf		PORTA
+
+wrap		movlw		39
+		movwf		temp
+decw		decf		temp, f
+		bz		toggle
+		goto		outer
+		
+outer		movlw		255
+		movwf		temp1
+dec1		decf		temp1, f
+		bz		decw
+rc		rcall		inner
+		goto		dec1
+inner		movlw		255
+		movwf		temp2
+dec		decf		temp2, f
+		bnz		dec
+		return
+
+toggle		btg		PORTA, RA2
+		goto 		wrap
+stop		goto		stop
+
+		end
